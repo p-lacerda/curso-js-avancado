e0dacf5c65667390b35a3a72da340abdac22b037
p-lacerda <xcloudgameemail@gmail.com>
2021-08-22 18:14:03 -0300

    Termina o conteudo da aula 6

---
+++ secao_2/aula6/index.js
@@ -0,0 +1,27 @@
+let nome = 'João';
+
+/* console.log(`${nome} nasceu em 1984.`)
+console.log(`Em 2000 ${nome} conheceu Maria.`)
+console.log(`${nome} casou-se com Maria em 2012.`)
+console.log(`Mariam teve 1 filho com ${nome} em 2015.`)
+console.log(`O filho de ${nome} se chama Eduardo.`) */
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+
+let nomeClienteJoao = 'João';
+// console.log(nomeClienteJoao);
+
+// Não pode começar o nome de uma variável com um número
+let no1me;
+// Não podem conter espaços ou traços
+let nomeCompleto = 'Luiz Otávio';
+// console.log(nomeCompleto);
+let nome_completo;
+// console.log(nome_completo);
+
+// Case-sensitive
+let nomeCliente = 'Luiz';
+let nomecliente = 'Otávio';
+
+console.log(nomeCliente, nomecliente);
