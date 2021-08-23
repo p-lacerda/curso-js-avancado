ef4b0ea76a25606001683c45dfa8828450846f28
p-lacerda <xcloudgameemail@gmail.com>
2021-08-23 02:30:25 -0300

    Adiciona os arquivos da aula 9

---
+++ secao_2/aula9/index.js
@@ -0,0 +1,14 @@
+// ECMAScript 2015 (ES6) Javascript
+let variavel;
+//
+nome = 'Luiz'; // NÃO FAÇA ISSO
+//
+var nome = 'Luiz'; 
+var nome = 'Otávio';
+
+console.log(nome);
+
+/*
+Var => Você consegue redeclarar variáveis e elas escapam do escopo
+Let => Você não consegue redeclarar e elas não vazam
+*/
