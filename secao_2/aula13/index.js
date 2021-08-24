33c3e76292ce73f1411b7304c2309d89e4135fdc
p-lacerda <xcloudgameemail@gmail.com>
2021-08-24 18:20:32 -0300

    Adiciona arquivos a aula 13

---
+++ secao_2/aula13/index.js
@@ -0,0 +1,17 @@
+// let umaString = 'Um "texto"'
+let umaString = "Um texto"
+
+// console.log(umaString);
+// console.log(umaString[4]);
+// console.log(umaString.charAt(6));
+// console.log(umaString.concat(` em um lindo dia!`));
+// console.log(umaString.indexOf('texto'));
+// console.log(umaString.lastIndexOf('m', 3));
+// console.log(umaString.match(/[a-z]/g)); // Expressões regulares
+// console.log(umaString.search(/o/));
+// console.log(umaString.replace('Um', 'outra'));
+
+let outraString = "O rato roeu a roupa do rei de roma."
+console.log(outraString.slice(-5));
+console.log(outraString.split(' '));
+
