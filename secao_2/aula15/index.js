6d1ad6afeee4b30dc96b6c5b40423afd7055c085
p-lacerda <xcloudgameemail@gmail.com>
2021-08-24 19:10:21 -0300

    Adiciona arquivos a aula 15

---
+++ secao_2/aula15/index.js
@@ -0,0 +1,11 @@
+let num1 = 9.54578;
+
+console.log(!!(100 / 0))
+// let num2 = Math.floor(num1); // ARREDONDA PRA BAIXO
+// let num2 = Math.ceil(num1); // ARREDONDA PRA CIMA
+// let num2 = Math.round(num1); // ARREDONDA PRA BAIXO OU PRA CIMA
+
+// console.log(Math.min(1, 19, 92, 91, 1000, 29, 10001));
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+
+console.log(Math.PI);
