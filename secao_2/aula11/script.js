008d69cfd4f46507e0290d5e93ac96b561e477fa
p-lacerda <xcloudgameemail@gmail.com>
2021-08-24 02:33:10 -0300

    Adiciona arquivos da aula 11

---
+++ secao_2/aula11/script.js
@@ -0,0 +1,11 @@
+// alert('Alguma caixinha')
+
+// window.confirm('Deseja realmente apagar?') // Confirmação
+
+// window.prompt('Digite o seu nome: ') // Input
+
+const a = window.prompt('Digite algum numero');
+const b = window.prompt('Digite outro numero');
+const resultado = Number(a) + Number(b);
+
+alert(`O resultado da soma foi ${resultado}`)
