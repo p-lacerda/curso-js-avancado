b8937059e961fba2556bdab93a04acfdf7510bab
p-lacerda <xcloudgameemail@gmail.com>
2021-08-24 18:48:14 -0300

    Adiciona arquivos da aula 14

---
+++ secao_2/aula14/index.js
@@ -0,0 +1,19 @@
+// IEEE 754-2008
+
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 = Number(num1.toFixed(2));
+
+
+let num1 = 10.25; // number
+let num2 = 2.5; // number
+
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+// console.log(num1.toString(2)); // TRANSFORMA EM BINARIO
+// console.log(num1.toFixed(2));
+// console.log(Number.isInteger(num1)); // É INTEIRO?
+
+// let temp = num1 * '5';
+// console.log(Number.isNaN(temp)) // É NaN?
