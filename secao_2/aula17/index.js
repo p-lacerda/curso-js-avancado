f3a4e07a8f2020b67a8ea7ac8033cbba8828d63d
p-lacerda <xcloudgameemail@gmail.com>
2021-08-25 01:45:17 -0300

    Adiciona arquivos a aula 17

---
+++ secao_2/aula17/index.js
@@ -0,0 +1,38 @@
+arrayNomes = ['Paulo', 'Maria', 'Eduardo']
+
+// function saudacao(array) {
+//   array.forEach((element) => {
+//     console.log(`Bom dia, ${element}!`)
+//   });
+// }
+
+//---------------------------
+
+// Função
+// function saudacao(element) {
+//   return `Bom dia, ${element}!`
+// }
+
+// const variavel = saudacao('Paulo')
+// console.log(variavel)
+
+//---------------------------
+
+
+// Arrow Functions
+
+// const soma = (x = 1 , y = 1) => {
+//   const resultado = x + y;
+//   return resultado
+// } 
+
+// console.log(soma(4, 5))
+
+// const resultado = soma(2, 2);
+// console.log(resultado);
+
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
