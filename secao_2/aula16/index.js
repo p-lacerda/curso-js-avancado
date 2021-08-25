1fbd7d1d30e5cace0027b40b5164c287f0d91310
p-lacerda <xcloudgameemail@gmail.com>
2021-08-25 00:32:26 -0300

    Adiciona arquivos a aula 16

---
+++ secao_2/aula16/index.js
@@ -0,0 +1,33 @@
+// Array é uma lista de coisas
+let nomes = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo
+//          0: 'Luiz', 1: 'Maria', 2: 'João'
+// nomes = 123
+
+console.log(typeof nomes);
+console.log(nomes instanceof Array); // É um array?
+
+// nomes.push('Luiza');
+// nomes.push('Eduardo');
+// console.log(nomes.slice(0, 3));
+// console.log(nomes.slice(0, -2));
+
+// console.log(nomes);
+
+// const removido = nomes.pop(); // Remove do final
+// const removido = nomes.shift(); // Remove do começo
+// console.log(removido)
+
+// nomes.unshift('Luiza') // Adiciona no começo
+// nomes.push('Otávio') // Adiciona no fim
+
+// nomes[nomes.length] = 'Luiza'
+// nomes[nomes.length] = 'Fábio'
+// nomes[nomes.length] = 'Luana'
+
+// nomes[0] = 'Eduardo';
+// nomes[3] = 'Luiza';
+
+
+// console.log(nomes);
+// console.log(nomes[0]);
+// console.log(nomes[2]);
